6d0c638de1185385d23114bcf907b377b2234533
Marcus Lerro <mjlerro@ncsu.edu>
2021-09-24 13:40:57 -0400

    commiting files to personal repo

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3030
+var bodyParser = require('body-parser');
+const { response } = require('express');
 
 // Serve static html/js/css content publically
 app.use(express.static('www'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const users = [
     "Addison", "Ful", "Mouad", "Devon", "Michael",
@@ -18,6 +24,15 @@ const users = [
     "Brian", "Alex", "TJ", "Travis", "Eve", "Zhonghe", "Tyler"
 ];
 
+var receiver = "";
+var payment = "";
+var ccname = "";
+var ccnumber = "";
+var ccmonth = "";
+var ccyear = "";
+var cvv = "";
+var preview = "";
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -27,7 +42,39 @@ app.get('/users', (req, res) => {
     console.log("/users");
     res.json(users);
 })
-  
+
+app.get('/pay-info', (req, res) => {
+  const myHeaders = new Headers();
+  myHeaders.append('receiver', receiver);
+  myHeaders.append('payment', payment);
+  myHeaders.append('ccnumber', ccnumber);
+  myHeaders.append('ccmonth', ccmonth);
+  myHeaders.append('ccyear', ccyear);
+  res.json(myHeaders);
+})
+
+app.post('/submit-form', (req, res) => {
+  receiver = req.body.users;
+  payment = req.body.payment;
+  ccname = req.body.ccname;
+  ccnumber = req.body.ccnumber;
+  ccmonth = req.body.ccmonth;
+  ccyear = req.body.ccyear;
+  cvv = req.body.cvv;
+  preview = req.body.preview;
+
+  var lastFour = ccnumber.substr(ccnumber.length - 4);
+  var lastTwo = ccyear.substr(ccyear.length - 2);
+
+  res.write(`Transaction Successful!\n\n`);
+  res.write(`Receiver: ` + receiver + `\n`);
+  res.write(`Payment: $` + payment + `\n`);
+  res.write(`CC Number: **** **** **** ` + lastFour + `\n`);
+  res.write(`CC Expiration: ` + ccmonth + `/` + lastTwo  + `\n`);
+  res.send();
+
+  //res.sendFile(path.join(__dirname, '/www/pay.html'));
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
